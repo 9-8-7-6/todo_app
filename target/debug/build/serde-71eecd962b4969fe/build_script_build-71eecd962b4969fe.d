4cd7873984d1c13c7a42478f4b863993b0a9d436
vito <ac1246897@gmail.com>
2025-01-31 12:07:32 +0800

    delete files in target directory

--- target/debug/build/serde-71eecd962b4969fe/build_script_build-71eecd962b4969fe.d
+++
@@ -1,5 +0,0 @@
-/Users/linyouyi/Desktop/todo_app/target/debug/build/serde-71eecd962b4969fe/build_script_build-71eecd962b4969fe: /Users/linyouyi/.cargo/registry/src/index.crates.io-6f17d22bba15001f/serde-1.0.217/build.rs
-
-/Users/linyouyi/Desktop/todo_app/target/debug/build/serde-71eecd962b4969fe/build_script_build-71eecd962b4969fe.d: /Users/linyouyi/.cargo/registry/src/index.crates.io-6f17d22bba15001f/serde-1.0.217/build.rs
-
-/Users/linyouyi/.cargo/registry/src/index.crates.io-6f17d22bba15001f/serde-1.0.217/build.rs:
--- target/debug/build/serde_json-ad83594f9b72c22f/build_script_build-ad83594f9b72c22f.d
+++
@@ -1,5 +0,0 @@
-/Users/linyouyi/Desktop/todo_app/target/debug/build/serde_json-ad83594f9b72c22f/build_script_build-ad83594f9b72c22f: /Users/linyouyi/.cargo/registry/src/index.crates.io-6f17d22bba15001f/serde_json-1.0.138/build.rs
-
-/Users/linyouyi/Desktop/todo_app/target/debug/build/serde_json-ad83594f9b72c22f/build_script_build-ad83594f9b72c22f.d: /Users/linyouyi/.cargo/registry/src/index.crates.io-6f17d22bba15001f/serde_json-1.0.138/build.rs
-
-/Users/linyouyi/.cargo/registry/src/index.crates.io-6f17d22bba15001f/serde_json-1.0.138/build.rs:
