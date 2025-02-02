c6b271e0c5ad2009e8da8e144fe513098eaae537
vito <vito@gmail.com>
2025-02-02 22:05:16 +0800

    modify entry point in main.rs to use cli to interact with these trait

--- src/main.rs
+++ src/main.rs
@@ -7,30 +7,26 @@ use serde_json::{json, Map};
 use state::{read_file, write_to_file};
 use std::env;
 
+use processes::process_input;
 use to_do::enums::TaskStatus;
 use to_do::to_do_factory;
 use to_do::traits::{delete::Delete, edit::Edit, get::Get};
 use to_do::ItemTypes;
 
 fn main() {
-    // let to_do_items = to_do_factory("washing", TaskStatus::DONE);
-    // match to_do_items {
-    //     ItemTypes::Done(item) => {
-    //         item.get(&item.super_struct.title);
-    //         item.delete(&item.super_struct.title);
-    //     }
-    //     ItemTypes::Pending(item) => {
-    //         item.get(&item.super_struct.title);
-    //         item.set_to_done(&item.super_struct.title);
-    //     }
-    // }
-
     let args: Vec<String> = env::args().collect();
-    let status: &String = &args[1];
+    let command: &String = &args[1];
     let title: &String = &args[2];
-    let mut state: Map<String, Value> = read_file("./state.json");
-    println!("Before operation: {:?}", state);
-    state.insert(title.to_string(), json!(status));
-    println!("After operation: {:?}", state);
-    write_to_file("./state.json", &mut state);
+    let state: Map<String, Value> = read_file("./state.json");
+    let status: String;
+    match &state.get(*&title) {
+        Some(result) => {
+            status = result.to_string().replace('\"', "");
+        }
+        None => {
+            status = "pending".to_owned();
+        }
+    }
+    let item = to_do_factory(title, TaskStatus::from_string(status.to_uppercase()));
+    process_input(item, command.to_string(), &state);
 }
